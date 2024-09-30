cd9b080f3d875e9edc280b24c51a3574552e713e
Solomon Antony <santony@murraystate.edu>
2024-09-30 11:36:38 -0500

    end of class 9/30

--- 01_files_from_dir.py
+++ 01_files_from_dir.py
@@ -2,7 +2,7 @@ import os
 def printdir(dir):
   filenames = os.listdir(dir)
   for filename in filenames:
-    print('file name: ',filename)  ## foo.txt
-    #print('os path:', os.path.join(dir, filename)) ## dir/foo.txt (relative to current dir)
+    #print('file name: ',filename)  ## foo.txt
+    print('os path:', os.path.join(dir, filename)) ## dir/foo.txt (relative to current dir)
     #print(os.path.abspath(os.path.join(dir, filename))) ## /home/nick/dir/foo.txt
-printdir('C:/Users/santony/Documents/')
+printdir('C:/Users/santony/Desktop')
